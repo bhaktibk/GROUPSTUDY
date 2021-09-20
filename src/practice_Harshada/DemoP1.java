0e1e247a9c6305fa1971254aac0b0a560b46c34d
Harshada Mandhare <harshadamandhare01@gmail.com>
2021-09-20 22:12:41 +0530

    Harshada_P1

---
+++ src/practice_Harshada/DemoP1.java
@@ -0,0 +1,10 @@
+package practice_Harshada;
+
+public class DemoP1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Test Program");
+	}
+
+}
